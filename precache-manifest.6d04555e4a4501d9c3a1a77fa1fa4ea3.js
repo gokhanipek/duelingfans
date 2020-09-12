b5d4d2398caf8fc78190a109da84ebc1a5956313
Gokhan <gipek03@gmail.com>
2020-09-13 01:21:56 +0300

    Updates

---
+++ precache-manifest.6d04555e4a4501d9c3a1a77fa1fa4ea3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b0885d30f62d923719a3863f33145f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b00b7df0b3339126c19",
+    "url": "./static/css/main.5cf08401.chunk.css"
+  },
+  {
+    "revision": "823246fc55daf7b4bb01",
+    "url": "./static/js/2.7e8c9bb6.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "./static/js/2.7e8c9bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b00b7df0b3339126c19",
+    "url": "./static/js/main.0fe209dc.chunk.js"
+  },
+  {
+    "revision": "aa8ee7eda10a530aec53",
+    "url": "./static/js/runtime-main.871dee85.js"
+  },
+  {
+    "revision": "0a12e54a99e931efaa879d7fb87c4fe3",
+    "url": "./static/media/bg.0a12e54a.png"
+  },
+  {
+    "revision": "6a30af2b3aa4beb57a581458f7c59e08",
+    "url": "./static/media/curry.6a30af2b.png"
+  },
+  {
+    "revision": "61b73df9c65d26e7d2abdf313e522644",
+    "url": "./static/media/dueling-fans-inner-white.61b73df9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/duelingfans/precache-manifest.96a9971a6ac3fb9a23b93f2fbefa3bf1.js"
+  "./precache-manifest.6d04555e4a4501d9c3a1a77fa1fa4ea3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/duelingfans/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
